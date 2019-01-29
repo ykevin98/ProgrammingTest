29820b235ff0c8c6e7c4878a4bd8ec0f80de3822
Kevin Yu <ykevin98@gmail.com>
2019-01-29 15:40:45 -0600

    Answered objective 4

--- DNA_encoding.cpp
+++ DNA_encoding.cpp
@@ -60,6 +60,56 @@ string encodeDNA(string str) {
 	return combine;
 }
 
+string decryptDNA(string str) {
+	string DNA = str;
+	string binary = "";
+	for (int i = 0; i < str.length(); i++) {
+		char curr = DNA.at(i);
+		if (curr == 'A') {
+			binary += "00";
+		}
+		else if (curr == 'T') {
+			binary += "01";
+		}
+		else if (curr == 'G') {
+			binary += "10";
+		}
+		else if (curr == 'C') {
+			binary += "11";
+		}
+	}
+	return binary;
+}
+
+int binaryCalculator(string str) {
+	/* Calculates the decimal value of the binary number */
+	int value = 0;
+	int indexCounter = 0;
+	for (int i = str.length() - 1; i > -1; i--) {
+		char curr = str.at(i);
+		if (curr == '1') {
+			value += pow(2, indexCounter);
+		}
+		indexCounter++;
+	}
+	cout << "The deciminal value of " << str << " is " << value << endl;
+	return value;
+}
+
+string returnStr(string str) {
+	/* returns the text string of a given DNA strand */
+	string temp = str;
+	string combine = "";
+	for (int i = 0; i < str.length(); i += 8) {
+		string current = str.substr(i, 8);
+		int integerValue = binaryCalculator(current);
+		char ASCIIVAlue = char(integerValue);
+		combine += ASCIIVAlue;
+	}
+	
+	return combine;
+}
+
 char toRNA(char DNA) {
 	/* converts one DNA nucleotide to RNA nucleotide */
 	switch (DNA) {
@@ -76,6 +126,23 @@ char toRNA(char DNA) {
 	}
 }
 
+char toDNA(char compDNA) {
+	/* converts one DNA nucleotide to match the other */
+	// A and T
+	// C and G
+	switch (compDNA) {
+	case 'A':
+		return 'T';
+	case 'T':
+		return 'A';
+	case 'C':
+		return 'G';
+	case 'G':
+		return 'C';
+	default:
+		return compDNA;
+	}
+}
 string encodeRNA(string str) {
 	/* converts the string from DNA to RNA */
 	string RNA = "";
@@ -83,6 +150,15 @@ string encodeRNA(string str) {
 		RNA += toRNA(str.at(i));
 	}
 	return RNA;
+}
+
+string inverseDNA(string str) {
+	/* creates a second strand of DNA that mataches every A to T and G to C */
+	string DNA = "";
+	for (int i = 0; i < str.length(); i++) {
+		DNA += toDNA(str.at(i));
+	}
+	return DNA;
 }
 
 int main() {
@@ -96,9 +172,14 @@ int main() {
 	cin >> choice;
 
 	string bin = toBinary(input);
+
+	/* converts the string to DNA */
 	string DNA = encodeDNA(bin); 
+	
+	/* encodes an exisiting DNA to RNA */
 	string RNA = encodeRNA(DNA);
 	//cout << "The binary value of " << bin << " is " << bin << endl;
+
 	if (choice == "1") {
 		cout << "The DNA encoder of " << input << " is " << DNA << endl;
 	}
@@ -106,6 +187,18 @@ int main() {
 		cout << "The RNA encoder of " << DNA << " is " << RNA << endl;
 	}
 
-	
+	/* Objective 4 */
+
+	string compDNA = "ACTGACTAAGAT"; // using the given DNA strand from the prompt
+	string newDNA = inverseDNA(compDNA);
+
+	cout << "The matched DNA to each nucleotide for " << compDNA << " is " << newDNA << endl;
+
+	string binary = decryptDNA(newDNA);
+	cout << "The binary number of " << newDNA << " is " << binary << endl;
+
+	string text = returnStr(binary);
+	cout << "The DNA strand is " << text << endl;
+
 	return 0;
 }
